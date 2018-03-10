4f78a61b9c0174838e05bc334c03ff3eb7b523d4
Cakoluchiam <joseph.strong@pearson.com>
2018-03-10 11:55:13 -0800

    barebones hero selector

--- thunderstone.js
+++ thunderstone.js
@@ -4,6 +4,14 @@ $(function() {
   var sets;
   var cards;
 
+  var hero_options;
+  var spell_options;
+  var item_options;
+  var weapon_options;
+  var monster_options;
+  var guardian_options;
+  var room_options;
+
   $.getJSON("enums.json",function(data){
     cs = data;
     log("Enums loaded:",cs);
@@ -16,16 +24,29 @@ $(function() {
 
   $.getJSON("cards.json",function(data){
     cards = data;
+    hero_options = cards.Heroes;
+    spell_options = cards.Spells;
+    item_options = cards.Items;
+    weapon_options = cards.Weapons;
+    monster_options = cards.Monsters;
+    guardian_options = cards.Guardians;
+    room_options = cards["Dungeon Rooms"];
     log("Cards loaded:",cards);
   });
 
-  $("#group_selector button").button();
+  $(".generate").button();
+
+  $("#choose_heroes .generate").click(function(event){
+    choices = [];
+    options = Array.from(Object.keys(hero_options));
+
+    while(choices.length < 4) {
+      choices.push(options.slice(Math.floor(Math.random()*options.length)));
+    }
+    console.log(choices);
+  });
+
   $("button").click(function(event){
     event.preventDefault();
-    log("hello world");
-  });
-  $("#group_select_base").click(function(event){
-    log("Selecting Base Set");
-    log(event);
   });
 });
